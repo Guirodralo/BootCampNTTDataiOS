7d272ca926152087aa46aefb66789f25e69584ed
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-02-20 17:34:08 +0100

    [WIP] -My first commit on github-

---
+++ 20_02_2024_iOS/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// -> Comentario de una linea
+
+/*
+ -> Comentario de varias lineas
+ */
+
+//MARK: - Tipos basicos
+
+var nombre: String = "pepe" // var -> define una propiedad mutable
+
+let apellidos: String = "ramírez" // var -> define una propiedad mutable
+
+//MARK: - Tipos basicos
