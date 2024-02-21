f5ac82c59f6c6d4f1e1dc37cae1ff1cf18f842c1
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-02-21 10:26:04 +0100

    [WIP] -Added new project to Repo-

---
+++ 21_02_2024_iOS/AppHolaMundo/AppHolaMundo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AppHolaMundo
+//
+//  Created by Guillermo Rodríguez ALonso on 21/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    MARK: - life VC
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//    MARK: - Extension VC
