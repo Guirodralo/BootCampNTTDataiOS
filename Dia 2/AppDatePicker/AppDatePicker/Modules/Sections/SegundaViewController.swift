b0722943d0433bfa5413c3e70343db95b5d4381d
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-02-22 13:58:06 +0100

    [WIP] -Added AppDatePicker-

--- Dia 2/AppDatePicker/AppDatePicker/Modules/Sections/SegundaViewController.swift
+++ Dia 2/AppDatePicker/AppDatePicker/Modules/Sections/SegundaViewController.swift
@@ -8,22 +8,86 @@
 import UIKit
 
 class SegundaViewController: UIViewController {
-
+    
+    //MARK: - Variables locales
+    var pickerQuesosArrayData = ["Mozzarella", "Gorgonzola", "Provolone", "Stilton", "Asiago"]
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var tituloQuesoLabel: UILabel!
+    @IBOutlet weak var detalleQuesoTextView: UITextView!
+    @IBOutlet weak var imagenQuesoImageView: UIImageView!
+    @IBOutlet weak var seleccionQuesoPickerView: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.seleccionQuesoPickerView.delegate = self
+        self.seleccionQuesoPickerView.dataSource = self
+        
+        self.title = pickerQuesosArrayData[0]
+        
+        self.tituloQuesoLabel.text = pickerQuesosArrayData[0]
+        
+        self.detalleQuesoTextView.text = "La mozzarella (en italiano: /mottsa'rɛlla/) o muzzarella (en napolitanoː /muttsa'rɛlla/), o también en castellano mozarela, muzarela, muzarella o musarela, es un queso semisuave, del sur de Italia elaborado tradicionalmente con leche de búfala italiana mediante el método de la pasta hilada, aunque internacionalmente está extendida la elaboración de quesos mozzarelle a base de leche de vaca, encontrándose en Campania y Mediodía como una especialidad tradicional garantizada (ETG), así como con leche de oveja en Cerdeña, los Abruzos y el Lacio. La mozzarella fresca es generalmente blanca, pero cuando se sazona se torna de color amarillo claro dependiendo de la dieta del animal. Debido a su alto contenido de humedad, tradicionalmente se sirve al día siguiente de su elaboración, sin embargo, se puede mantener en salmuera hasta por una semana o más cuando se vende en paquetes sellados al vacío. La mozzarella de baja humedad se puede mantener refrigerada hasta por un mes, aunque la mozzarella rallada con bajo contenido de humedad se vende con una vida útil de hasta seis meses. La mozzarella se usa para la mayoría de los tipos de pizza y varios platos de pasta o se sirve con rodajas de tomate y albahaca en la ensalada caprese."
+        
+        self.imagenQuesoImageView.image = UIImage(named: pickerQuesosArrayData[0])
 
-        // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SegundaViewController: UIPickerViewDelegate  {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerQuesosArrayData[row]
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 30
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        self.title = pickerQuesosArrayData[row]
+        self.tituloQuesoLabel.text = pickerQuesosArrayData[row]
+        self.imagenQuesoImageView.image = UIImage(named: pickerQuesosArrayData[row])
+        
+        switch row {
+        case 0:
+            self.detalleQuesoTextView.text = "La mozzarella (en italiano: /mottsa'rɛlla/) o muzzarella (en napolitanoː /muttsa'rɛlla/), o también en castellano mozarela, muzarela, muzarella o musarela, es un queso semisuave, del sur de Italia elaborado tradicionalmente con leche de búfala italiana mediante el método de la pasta hilada, aunque internacionalmente está extendida la elaboración de quesos mozzarelle a base de leche de vaca, encontrándose en Campania y Mediodía como una especialidad tradicional garantizada (ETG), así como con leche de oveja en Cerdeña, los Abruzos y el Lacio."
+            
+            
+        case 1:
+            
+            self.detalleQuesoTextView.text = "aunque internacionalmente está extendida la elaboración de quesos mozzarelle a base de leche de vaca, encontrándose en Campania y Mediodía como una especialidad tradicional garantizada (ETG), así como con leche de oveja en Cerdeña, los Abruzos y el Lacio."
+            
+        case 2:
+            self.detalleQuesoTextView.text = "a de quesos mozzarelle a base de leche de vaca, encontrándose en Campania y Mediodía como una especialidad tradicional garantizada (ETG), así como con leche de oveja en Cerdeña, los Abruzos y el Lacio."
+            
+        case 3:
+            self.detalleQuesoTextView.text = " a base de leche de vaca, encontrándose en Campania y Mediodía como una especialidad tradicional garantizada (ETG), así como con leche de oveja en Cerdeña, los Abruzos y el Lacio."
+        case 4:
+            self.detalleQuesoTextView.text = "a especialidad tradicional garantizada (ETG), así como con leche de oveja en Cerdeña, los Abruzos y el Lacio."
+            
+        default:
+            self.detalleQuesoTextView.text = " así como con leche de oveja en Cerdeña, los Abruzos y el Lacio."
+            
+        }
+    }
+    
+}
 
+extension SegundaViewController: UIPickerViewDataSource {
+    
+    // Devuelve el numero de componentes
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Devuelve el numero de filas por componentes
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerQuesosArrayData.count
+    }
 }
--- Dia 2/AppDatePicker/AppDatePicker/Modules/Sections/TerceraViewController.swift
+++ Dia 2/AppDatePicker/AppDatePicker/Modules/Sections/TerceraViewController.swift
@@ -8,22 +8,92 @@
 import UIKit
 
 class TerceraViewController: UIViewController {
-
+    //MARK: - Variables locales
+    var localidadesMadridDataSource = ["La Latina", "Salamanca", "Fuencarral", "Pepitoperez"]
+    var codigoPostalDataSource = ["28010", "28009", "28017", "28000"]
+    var prioridadDataSource = ["BAJA", "MEDIA", "ALTA", "MUY ALTA"]
+    var imagenesPerfilDataSource = ["felipe", "maria", "felipe", "maria"]
+    
+    
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var localidadMadrid: UITextField!
+    @IBOutlet weak var codigoPostalZona: UITextField!
+    @IBOutlet weak var prioridadDeTrabajo: UITextField!
+    @IBOutlet weak var imagenPerfilImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+                
+        self.imagenPerfilImageView.image = UIImage(named: imagenesPerfilDataSource[0])
+        
+        createPickerView(myTag: 0, myTextFieldCustom: localidadMadrid, myArray: localidadesMadridDataSource)
+        createPickerView(myTag: 1, myTextFieldCustom: codigoPostalZona, myArray: codigoPostalDataSource)
+        createPickerView(myTag: 2, myTextFieldCustom: prioridadDeTrabajo, myArray: prioridadDataSource)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createPickerView(myTag: Int, myTextFieldCustom: UITextField, myArray: [String]){
+        
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.tag = myTag
+        myTextFieldCustom.inputView = pickerView
+        myTextFieldCustom.text = myArray[0]
     }
-    */
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
 }
+
+extension TerceraViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 0:
+            return localidadesMadridDataSource.count
+        case 1:
+            return codigoPostalDataSource.count
+        default:
+            return prioridadDataSource.count
+        }
+    }
+}
+
+
+extension TerceraViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 0:
+            return localidadesMadridDataSource[row]
+        case 1:
+            return codigoPostalDataSource[row]
+        default:
+            return prioridadDataSource[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 0:
+            localidadMadrid.text = localidadesMadridDataSource[row]
+        case 1:
+            codigoPostalZona.text = codigoPostalDataSource[row]
+        default:
+            prioridadDeTrabajo.text = prioridadDataSource[row]
+            imagenPerfilImageView.image = UIImage(named: imagenesPerfilDataSource[row])
+        }
+    }
+}
+
