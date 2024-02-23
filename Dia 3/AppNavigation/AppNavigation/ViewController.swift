36adc137cd6304b2d5be608a60fef50593c7010f
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-02-23 09:29:22 +0100

    [WIP] -Init project-

---
+++ Dia 3/AppNavigation/AppNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNavigation
+//
+//  Created by Guillermo Rodríguez ALonso on 22/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
