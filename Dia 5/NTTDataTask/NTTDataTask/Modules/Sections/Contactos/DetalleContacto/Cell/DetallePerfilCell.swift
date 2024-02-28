7341abef9f26fbbb6780c48a15c406d0ceab5004
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-02-28 16:59:30 +0100

    [WIP] -Next steps of TaskApp-

---
+++ Dia 5/NTTDataTask/NTTDataTask/Modules/Sections/Contactos/DetalleContacto/Cell/DetallePerfilCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetallePerfilCell.swift
+//  NTTDataTask
+//
+//  Created by Guillermo Rodríguez ALonso on 28/2/24.
+//
+
+import UIKit
+
+class DetallePerfilCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
