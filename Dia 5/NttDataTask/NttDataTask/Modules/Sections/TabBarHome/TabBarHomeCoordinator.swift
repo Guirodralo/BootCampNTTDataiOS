8f6e2aa805ac4f3565157d87358207e57663a31b
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-02-28 08:43:29 +0100

    [WIP] -Started the AppTask-

---
+++ Dia 5/NttDataTask/NttDataTask/Modules/Sections/TabBarHome/TabBarHomeCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarHomeCoordinator.swift
+//  NttDataTask
+//
+//  Created by Guillermo Rodríguez ALonso on 27/2/24.
+//
+
+import Foundation
+import UIKit
+
+final class TabBarHomeCoordinator {
+    
+    
+    
+    static func view() -> UITabBarController {
+        let tbVC = TabBarHomeViewController()
+        return tbVC
+    }
+}
