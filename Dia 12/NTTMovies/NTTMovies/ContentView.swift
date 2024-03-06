7adde90e8d1095ae1e92ec0452736b03d7710766
Guillermo Rodríguez ALonso <grodralo@NTTD-C02FV0Z2ML85.local>
2024-03-06 13:56:28 +0100

    [WIP] -Started SwiftUI-

---
+++ Dia 12/NTTMovies/NTTMovies/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NTTMovies
+//
+//  Created by Guillermo Rodríguez ALonso on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding(EdgeInsets(top: 30, leading: 20, bottom: 0, trailing: 20))
+            Text("Hello, world!")
+                .padding(EdgeInsets(top: 30, leading: 20, bottom: 0, trailing: 20))
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dia 12/NTTMovies/NTTMovies/NTTMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTTMoviesApp.swift
+//  NTTMovies
+//
+//  Created by Guillermo Rodríguez ALonso on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct NTTMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
